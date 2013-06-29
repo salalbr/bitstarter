1e8d0a464dce8b41a5e6eec8073f9ec35b3b0dc5
salalbr <salalbr@yahoo.com.br>
2013-06-29 09:16:16 -0600

    Changed to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+/*
+var file = fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  var buf = new Buffer(data, 'utf8');
+  console.log(buf.toString());
+});
+*/
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.read('index.html', function (err, data) {                                                             
+    if (err) throw err;                                                                                                    var buf = new Buffer(data, 'utf8');
+    response.send(buf.toString());
+    //response.send('Hello World 2!');
+  });
 });
 
 var port = process.env.PORT || 5000;
